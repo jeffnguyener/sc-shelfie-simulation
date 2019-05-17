d140a76e6643264263b1447d0f311db4b98eb2f2
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro-2.local>
2019-05-17 10:33:00 -0600

    update db & server running

--- server/index.js
+++ server/index.js
@@ -3,10 +3,13 @@ const massive = require('massive')
 const express = require('express');
 const app = express();
 const control = require('./controller');
-
-
+const { SERVER_PORT, CONNECTION_STRING } = process.env
 
 app.use(express.json())
 
-const PORT = 4000
-app.listen(PORT, () => console.log(`Cruising on port....${PORT}`)) 
+massive(CONNECTION_STRING).then((database) => {
+    app.set('db', database)
+    console.log('Database 👌!')
+})
+
+app.listen(SERVER_PORT, () => console.log(`Cruising on port....${SERVER_PORT}`)) 
