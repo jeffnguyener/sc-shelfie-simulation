cfec1526e8880c978750f6928f2bad120d2ef30d
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro-2.local>
2019-05-17 15:37:14 -0600

    update v4

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -8,7 +8,11 @@ class Form extends Component {
 
         return (
             <div>
-               FORM
+                <div className="form">
+                <div className="box-uploader">
+                    <img src="https://cdn0.iconfinder.com/data/icons/interface-10/128/_add_image-512.png"></img>
+                </div>
+                </div>
             </div>
         )
     }
